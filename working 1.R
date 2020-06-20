17c67848be74e52aef8dd768eadc8396636a6ca6
rhizon531 <rhizon531@gmail.com>
2020-06-20 20:58:54 +0800

    Update working 1.R

--- working 1.R
+++ working 1.R
@@ -1,9 +1,10 @@
+rm(list = ls())
+
+
 ## packages
 
 library(dplyr)
 library(pmdplyr)
-library(readxl)
-library(haven)
 library(tidyr)
 library(sjlabelled)
 
@@ -11,10 +12,10 @@ library(sjlabelled)
 ## attempt 1
 
 setwd("~/Desktop/voting data")
-ZA3521_v2_0_1 <- read_sav("ZA3521_v2-0-1.sav")
-dataset2 <- ZA3521_v2_0_1
+dataset2 <- read_sav("ZA3521_v2-0-1.sav")
 
-ZA3648 <- read_data(
+
+dataset1 <- read_data(
   "~/Desktop/voting data/ZA3648.sav",
   atomic.to.fac = FALSE,
   drop.labels = FALSE,
@@ -22,7 +23,7 @@ ZA3648 <- read_data(
   verbose = FALSE
 )
 
-dataset1 <- ZA3648
+
 
 
 ## keeping variables ZA3521
@@ -33,33 +34,181 @@ dataset2$country_year <- paste(dataset2$nation2, dataset2$year, sep="_")
 dataset2 <- dataset2 %>% select(country_year, id, split, eb, better, econpast, finapast, satisdmo, closepty, feelclo, voteint, inclvote, lastvote, particip, lrs, party, married, educ, sex, age, sizehh, occup, soclass, income, regionat)
 View(dataset2)
 
-## ZA3648 - i will come back to this like it's stressed me out
+## ZA3648 - 
 
-dataset1$V3 <- factor(dataset1$V3, levels = c(1:22), labels = c("Albania", "Armenia", "Belarus", "Bulgaria", "Croatia", "CzechRepublic", "Slovakia", "Estonia", "Georgia", "Hungary", "Latvia", "Lithuania", "Macedonia", "Moldova", "Poland", "Romania", "Russia", "Slovenia", "Ukraine", "Kazakhstan", "Yugoslavia", "GDR"))
-dataset1$country_year <- paste(dataset1$V3, dataset1$V4,sep="_")
-
-datasubset1 <-  dataset1 %>% select(country_year, V80, V81, V82, V83, V84, V85, V86, V87, V88, V89, V90, V91, V92, V93, V94, V95, V96, V97, V98, V99, V100)
-
-datasubset1[is.na(datasubset1)] <- 0
-
-datasubset1 %>% mutate(sum = rowSums(.[V80:V100]))
-
-datasubset1$voteint <- paste(datasubset1$V80, datasubset1$V81, datasubset1$V82, datasubset1$V83, datasubset1$V84, datasubset1$V85, datasubset1$V86, datasubset1$V87, datasubset1$V88, datasubset1$V89,datasubset1$V90, datasubset1$V91, datasubset1$V92, datasubset1$V93, datasubset1$V94, datasubset1$V95, datasubset1$V96, datasubset1$V97, datasubset1$V98, datasubset1$V99, datasubset1$V100)
-
-dataset1 <-merge(dataset1, datasubset1, by="country_year")
-
-dataset1 <- dataset1 %>% select(country_year, V3V80:V100)
+dataset1$country <- factor(dataset1$V3, levels = c(1:22), labels = c("Albania", "Armenia", "Belarus", "Bulgaria", "Croatia", "CzechRepublic", "Slovakia", "Estonia", "Georgia", "Hungary", "Latvia", "Lithuania", "Macedonia", "Moldova", "Poland", "Romania", "Russia", "Slovenia", "Ukraine", "Kazakhstan", "Yugoslavia", "GDR"))
+dataset1$country_year <- paste(dataset1$country, dataset1$V4,sep="_")
 
 
-dataset1$voteint <- paste(dataset1$V80+ dataset1$V81+ dataset1$V82+dataset1$V83+ dataset1$V84+ dataset1$V85+ dataset1$V86+ dataset1$V87+ dataset1$V88+dataset1$V89+ dataset1$V90+ dataset1$V91+ dataset1$V92+ dataset1$V93+dataset1$V94+ dataset1$V95+dataset1$V96+ dataset1$V97+ dataset1$V98+ dataset1$V99+ dataset1$V100, na.rm = TRUE)
+#start by creating new variable for each V79:V100
+
+dataset1<-dataset1 %>% mutate(vote_Albania=V79)
+dataset1<-dataset1 %>% mutate(vote_Armenia=V80)
+dataset1<-dataset1 %>% mutate(vote_Belarus=V81)
+dataset1<-dataset1 %>% mutate(vote_Bulgaria=V82)
+dataset1<-dataset1 %>% mutate(vote_Croatia=V83)
+dataset1<-dataset1 %>% mutate(vote_Czech=V84)
+dataset1<-dataset1 %>% mutate(vote_Slovakia=V85)
+dataset1<-dataset1 %>% mutate(vote_Estonia=V86)
+dataset1<-dataset1 %>% mutate(vote_Hungary=V87)
+dataset1<-dataset1 %>% mutate(vote_Latvia=V88)
+dataset1<-dataset1 %>% mutate(vote_Lithuania=V89)
+dataset1<-dataset1 %>% mutate(vote_Macedonia=V90)
+dataset1<-dataset1 %>% mutate(vote_Poland=V91)
+dataset1<-dataset1 %>% mutate(vote_Romania=V92)
+dataset1<-dataset1 %>% mutate(vote_Russia=V93)
+dataset1<-dataset1 %>% mutate(vote_Slovenia=V94)
+dataset1<-dataset1 %>% mutate(vote_Ukraine=V95)
+dataset1<-dataset1 %>% mutate(vote_Georgia=V96)
+dataset1<-dataset1 %>% mutate(vote_Kazachstan=V97)
+dataset1<-dataset1 %>% mutate(vote_Yugoslavia=V98)
+dataset1<-dataset1 %>% mutate(vote_GDR=V99)
+dataset1<-dataset1 %>% mutate(vote_Moldova=V100)
 
 
-dataset1 %>% unite("voteint", V80:V100, na.rm = TRUE, remove = FALSE)
 
+## Replace NAs to 0
+dataset1$vote_Albania<-replace(dataset1$vote_Albania, is.na(dataset1$vote_Albania), 0)
+dataset1$vote_Armenia<-replace(dataset1$vote_Armenia, is.na(dataset1$vote_Armenia), 0)
+dataset1$vote_Belarus<-replace(dataset1$vote_Belarus, is.na(dataset1$vote_Belarus), 0)
+dataset1$vote_Bulgaria<-replace(dataset1$vote_Bulgaria, is.na(dataset1$vote_Bulgaria), 0)
+dataset1$vote_Croatia<-replace(dataset1$vote_Croatia, is.na(dataset1$vote_Croatia), 0)
+dataset1$vote_Czech<-replace(dataset1$vote_Czech, is.na(dataset1$vote_Czech), 0)
+dataset1$vote_Slovakia<-replace(dataset1$vote_Slovakia, is.na(dataset1$vote_Slovakia), 0)
+dataset1$vote_Estonia<-replace(dataset1$vote_Estonia, is.na(dataset1$vote_Estonia), 0)
+dataset1$vote_Hungary<-replace(dataset1$vote_Hungary, is.na(dataset1$vote_Hungary), 0)
+dataset1$vote_Latvia<-replace(dataset1$vote_Latvia, is.na(dataset1$vote_Latvia), 0)
+dataset1$vote_Lithuania<-replace(dataset1$vote_Lithuania, is.na(dataset1$vote_Lithuania), 0)
+dataset1$vote_Macedonia<-replace(dataset1$vote_Macedonia, is.na(dataset1$vote_Macedonia), 0)
+dataset1$vote_Poland<-replace(dataset1$vote_Poland, is.na(dataset1$vote_Poland), 0)
+dataset1$vote_Romania<-replace(dataset1$vote_Romania, is.na(dataset1$vote_Romania), 0)
+dataset1$vote_Russia<-replace(dataset1$vote_Russia, is.na(dataset1$vote_Russia), 0)
+dataset1$vote_Slovenia<-replace(dataset1$vote_Slovenia, is.na(dataset1$vote_Slovenia), 0)
+dataset1$vote_Ukraine<-replace(dataset1$vote_Ukraine, is.na(dataset1$vote_Ukraine), 0)
+dataset1$vote_Georgia<-replace(dataset1$vote_Georgia, is.na(dataset1$vote_Georgia), 0)
+dataset1$vote_Kazachstan<-replace(dataset1$vote_Kazachstan, is.na(dataset1$vote_Kazachstan), 0)
+dataset1$vote_Yugoslavia<-replace(dataset1$vote_Yugoslavia, is.na(dataset1$vote_Yugoslavia), 0)
+dataset1$vote_GDR<-replace(dataset1$vote_GDR, is.na(dataset1$vote_GDR), 0)
+dataset1$vote_Moldova<-replace(dataset1$vote_Moldova, is.na(dataset1$vote_Moldova), 0)
+
+
+
+# Add all relevant columns to get the sum; if NA then you get 0 otherwise the value of the party
+#dataset1$vote_int <- (dataset1$vote_Albania+dataset1$vote_Armenia+dataset1$vote_Belarus+dataset1$V82+dataset1$V83+dataset1$V84+dataset1$V85+dataset1$V86+dataset1$V87+dataset1$V88+dataset1$V89+dataset1$V90+dataset1$V91+dataset1$V92+dataset1$V93+dataset1$V94+dataset1$V95+dataset1$V96+dataset1$V97+dataset1$V98+dataset1$V99+dataset1$V100)
+
+
+dataset1$vote_int = (dataset1$vote_Albania+dataset1$vote_Armenia+dataset1$vote_Belarus+dataset1$vote_Bulgaria+dataset1$vote_Croatia+dataset1$vote_Czech+dataset1$vote_Slovakia+dataset1$vote_Estonia+dataset1$vote_Hungary+dataset1$vote_Latvia+dataset1$vote_Lithuania+dataset1$vote_Macedonia+dataset1$vote_Poland+dataset1$vote_Romania+dataset1$vote_Russia+dataset1$vote_Slovenia+dataset1$vote_Ukraine+dataset1$vote_Georgia+dataset1$vote_Kazachstan+dataset1$vote_Yugoslavia+dataset1$vote_GDR+dataset1$vote_Moldova) 
+
+# Test by country
+table(dataset1$country,dataset1$vote_int)
+View(dataset1)
+
+
+#Replace 0s to NAs and no vote/spoil. no answer
+
+dataset1$vote_int[dataset1$vote_int==0] <- NA
+dataset1$vote_int[dataset1$vote_Albania>=24] <- NA
+dataset1$vote_int[dataset1$vote_Armenia>=19] <- NA
+dataset1$vote_int[dataset1$vote_Belarus>=28] <- NA
+dataset1$vote_int[dataset1$vote_Bulgaria>=21] <- NA
+dataset1$vote_int[dataset1$vote_Croatia >=14] <- NA
+dataset1$vote_int[dataset1$vote_Czech>=31] <- NA
+dataset1$vote_int[dataset1$vote_Slovakia>=28] <- NA
+dataset1$vote_int[dataset1$vote_Estonia>=38] <- NA
+dataset1$vote_int[dataset1$vote_Hungary>=17] <- NA
+dataset1$vote_int[dataset1$vote_Latvia>=29] <- NA
+dataset1$vote_int[dataset1$vote_Lithuania>=28] <- NA
+dataset1$vote_int[dataset1$vote_Macedonia>=27] <- NA
+dataset1$vote_int[dataset1$vote_Poland>=39] <- NA
+dataset1$vote_int[dataset1$vote_Romania>=30] <- NA
+dataset1$vote_int[dataset1$vote_Russia>=39] <- NA
+dataset1$vote_int[dataset1$vote_Slovenia>=24] <- NA ## ND response?
+dataset1$vote_int[dataset1$vote_Ukraine>=25] <- NA
+dataset1$vote_int[dataset1$vote_Georgia>=28] <- NA
+dataset1$vote_int[dataset1$vote_Kazachstan>=23] <- NA
+dataset1$vote_int[dataset1$vote_Yugoslavia>=17] <- NA
+dataset1$vote_int[dataset1$vote_GDR>=17] <- NA
+dataset1$vote_int[dataset1$vote_Moldova>=17] <- NA
+
+table(dataset1$vote_int)
+table(dataset1$country,dataset1$vote_int)
+
+
+# Save the dataset
 save(dataset1, file="EAEurobarometer.RData")
 
 
+# mutating vote incl variables
 
+dataset1<-dataset1 %>% mutate(voteinc_Albania=V101)
+dataset1<-dataset1 %>% mutate(voteinc_Belarus=V102)
+dataset1<-dataset1 %>% mutate(voteinc_Bulgaria=V103)
+dataset1<-dataset1 %>% mutate(voteinc_Czech=V104)
+dataset1<-dataset1 %>% mutate(voteinc_Slovakia=V105)
+dataset1<-dataset1 %>% mutate(voteinc_Estonia=V106)
+dataset1<-dataset1 %>% mutate(voteinc_Hungary=V107)
+dataset1<-dataset1 %>% mutate(voteinc_Latvia=V108)
+dataset1<-dataset1 %>% mutate(voteinc_Lithuania=V109)
+dataset1<-dataset1 %>% mutate(voteinc_Macedonia=V110)
+dataset1<-dataset1 %>% mutate(voteinc_Poland=V111)
+dataset1<-dataset1 %>% mutate(voteinc_Romania=V112)
+dataset1<-dataset1 %>% mutate(voteinc_Russia=V113)
+dataset1<-dataset1 %>% mutate(voteinc_Slovenia=V114)
+dataset1<-dataset1 %>% mutate(voteinc_Ukraine=V115)
+dataset1<-dataset1 %>% mutate(voteinc_Georgia=V116)
+dataset1<-dataset1 %>% mutate(voteinc_Moldova=V117)
+
+
+# Replace NAs to 0s
+
+dataset1$voteinc_Albania<-replace(dataset1$voteinc_Albania, is.na(dataset1$voteinc_Albania), 0)
+dataset1$voteinc_Belarus<-replace(dataset1$voteinc_Belarus, is.na(dataset1$voteinc_Belarus), 0)
+dataset1$voteinc_Bulgaria<-replace(dataset1$voteinc_Bulgaria, is.na(dataset1$voteinc_Bulgaria), 0)
+dataset1$voteinc_Czech<-replace(dataset1$voteinc_Czech, is.na(dataset1$voteinc_Czech), 0)
+dataset1$voteinc_Slovakia<-replace(dataset1$voteinc_Slovakia, is.na(dataset1$voteinc_Slovakia), 0)
+dataset1$voteinc_Estonia-replace(dataset1$voteinc_Estonia, is.na(dataset1$voteinc_Estonia), 0)
+dataset1$voteinc_Hungary<-replace(dataset1$voteinc_Hungary, is.na(dataset1$voteinc_Hungary), 0)
+dataset1$voteinc_Latvia<-replace(dataset1$voteinc_Latvia, is.na(dataset1$voteinc_Latvia), 0)
+dataset1$voteinc_Lithuania<-replace(dataset1$voteinc_Lithuania, is.na(dataset1$voteinc_Lithuania), 0)
+dataset1$voteinc_Macedonia<-replace(dataset1$voteinc_Macedonia, is.na(dataset1$voteinc_Macedonia), 0)
+dataset1$voteinc_Poland<-replace(dataset1$voteinc_Poland, is.na(dataset1$voteinc_Poland), 0)
+dataset1$voteinc_Romania<-replace(dataset1$voteinc_Romania, is.na(dataset1$voteinc_Romania), 0)
+dataset1$voteinc_Russia<-replace(dataset1$voteinc_Russia, is.na(dataset1$voteinc_Russia), 0)
+dataset1$voteinc_Slovenia<-replace(dataset1$voteinc_Slovenia, is.na(dataset1$voteinc_Slovenia), 0)
+dataset1$voteinc_Ukraine<-replace(dataset1$voteinc_Ukraine, is.na(dataset1$voteinc_Ukraine), 0)
+dataset1$voteinc_Georgia<-replace(dataset1$voteinc_Georgia, is.na(dataset1$voteinc_Georgia), 0)
+dataset1$voteinc_Moldova<-replace(dataset1$voteinc_Moldova, is.na(dataset1$voteinc_Moldova), 0)
+
+
+# thingy
+
+dataset1$voteinc <- (dataset1$voteinc_Albania+dataset1$voteinc_Belarus+dataset1$voteinc_Bulgaria+dataset1$voteinc_Czech+dataset1$voteinc_Estonia+dataset1$voteinc_Hungary+dataset1$voteinc_Latvia+dataset1$voteinc_Lithuania+dataset1$voteinc_Macedonia+dataset1$voteinc_Poland+dataset1$voteinc_Romania+dataset1$voteinc_Russia+dataset1$voteinc_Slovenia+dataset1$voteinc_Ukraine+dataset1$voteinc_Georgia+dataset1$voteinc_Moldova)
+
+# Replace 0s to NAs
+
+#Replace 0s to NAs and no vote/spoil. no answer
+
+dataset1$voteinc[dataset1$voteinc==0] <- NA
+dataset1$voteinc[dataset1$voteinc_Albania>=24] <- NA
+dataset1$voteinc[dataset1$voteinc_Belarus>=28] <- NA
+dataset1$voteinc[dataset1$voteinc_Bulgaria>=21] <- NA
+dataset1$voteinc[dataset1$voteinc_Czech>=31] <- NA
+dataset1$voteinc[dataset1$voteinc_Slovakia>=28] <- NA
+dataset1$voteinc[dataset1$voteinc_Estonia>=38] <- NA
+dataset1$voteinc[dataset1$voteinc_Hungary>=17] <- NA
+dataset1$voteinc[dataset1$voteinc_Latvia>=29] <- NA
+dataset1$voteinc[dataset1$voteinc_Lithuania>=28] <- NA
+dataset1$voteinc[dataset1$voteinc_Macedonia>=27] <- NA
+dataset1$voteinc[dataset1$voteinc_Poland>=39] <- NA
+dataset1$voteinc[dataset1$voteinc_Romania>=30] <- NA
+dataset1$voteinc[dataset1$voteinc_Russia>=39] <- NA
+dataset1$voteinc[dataset1$voteinc_Slovenia>=24] <- NA ## ND response?
+dataset1$voteinc[dataset1$voteinc_Ukraine>=25] <- NA
+dataset1$voteinc[dataset1$voteinc_Georgia>=28] <- NA
+dataset1$voteinc[dataset1$voteinc_Moldova>=17] <- NA
+
+table(dataset1$voteinc)
+table(dataset1$country,dataset1$voteinc)
 
 
 
